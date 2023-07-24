7aca8b1942027c0b35fde4e43b0d0cb4e7f30f72
AdityaS98 <adityasharmalko1998@gmail.com>
2023-07-24 12:48:47 +0530

    Adding a program for calculating 100 hours and 20 days

---
+++ src/EmpEageUC6.java
@@ -0,0 +1,100 @@
+package com.problems;
+import java.util.*;
+
+public class EmpEageUC6 {
+    public static void main(String[] args) {
+        int salaryperhour = 80;
+        int totaldays = 0, totalhours = 0, totalsalary = 0, empworkhour = 0, salary = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        /*
+         * 2) Get the value from user for number of days
+         */
+
+        System.out.println("Enter Number of Days");
+        int NumofDays = sc.nextInt();
+
+        /*
+         * 3) For loop to check attendance of employee for number of days
+         */
+        int day = 1;
+
+        for (; day <= NumofDays; day++) {
+
+            /*
+             * 4) Calculate the random number
+             */
+            double num = Math.random() * 3;
+
+            /*
+             * 5) To convert random number into integer to use in switch case
+             */
+
+            Double newData = new Double(num);
+            int value = newData.intValue();
+
+            /*
+             * 6) To check the attendance of employee used switch case
+             */
+
+            switch (value) {
+                case 1:
+
+                    /*
+                     * 7) To Print if employee is present
+                     */
+
+                    empworkhour = 8;
+                    System.out.println("Employee is present");
+
+                    break;
+                case 2:
+
+                    /*
+                     * 8) To Print if employee is partialy present
+                     */
+
+                    empworkhour = 4;
+                    System.out.println("Employee is partialy present");
+
+                    break;
+                default:
+
+                    /*
+                     * 9) To Print if employee is absent
+                     */
+
+                    empworkhour = 0;
+                    System.out.println("Employee is absent");
+            }
+
+            /*
+             * 10) Put condition is given in problem statement number of days in month
+             * should be 20 and total working hours should 100
+             */
+
+            if (day <= 20 && totalhours <= 100) {
+
+                /*
+                 * 11) To calculate wage of employee
+                 */
+
+                salary = empworkhour * salaryperhour;
+
+                totalsalary = totalsalary + salary;
+                totalhours = totalhours + empworkhour;
+            }
+        }
+
+        /*
+         * 12) To display total salary of employee for month
+         */
+
+        System.out.println("Total salary of Employee "+totalsalary);
+        System.out.println("Total Working Hours "+totalhours);
+        System.out.println("Day number "+day);
+    }
+}
+
+
